9ebb389b21c335ca5b3de662d05d8a6324b193fe
Eduardo Banegas <87100421+EduBanegas@users.noreply.github.com>
2021-12-22 09:33:01 -0300

    Acomodando repo para mostrar en github

--- src/actions/addMovieFavorite .js
+++ src/actions/addMovieFavorite .js
@@ -5,8 +5,8 @@ import { ADD_MOVIE_FAVORITE } from "./index";
 // Exportamos la funcion encargada de hacer el request de los detalles de una pelicula a la api
 export default function addMovieFavorite(id) {
 
-    return {
-        type : ADD_MOVIE_FAVORITE,
+    return { 
+        type  : ADD_MOVIE_FAVORITE,
         payload : id
     }
 }
